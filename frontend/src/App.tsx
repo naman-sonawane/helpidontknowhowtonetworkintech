525e0940d646ab090f4843f9c615f1008a959f60
chrisma71 <chris.pengfei.ma@gmail.com>
2025-05-17 08:39:17 -0400

    tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,6 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="min-h-screen min-w-screen bg-cream flex flex-col justify-between items-center">
       {/* Main content */}
