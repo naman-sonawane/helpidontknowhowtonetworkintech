599d1baa1a982b1e28df2e61e08e3be889bb3590
chrisma71 <chris.pengfei.ma@gmail.com>
2025-05-17 01:34:50 -0400

    styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="min-h-screen bg-cream flex flex-col justify-between items-center">
+    <div className="min-h-screen min-w-screen bg-cream flex flex-col justify-between items-center">
       {/* Main content */}
       <div className="flex-1 flex flex-col items-center justify-center">
         {/* Text column */}
