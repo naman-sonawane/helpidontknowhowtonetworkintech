b355075de1e0b189ad96b9ca3ff9981898cde86f
naman-sonawane <sonawane.naman14@gmail.com>
2025-05-17 19:13:21 -0400

    build fixes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,13 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import AppRoutes from './routes'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <div className='min-w-screen'>
-        <AppRoutes />
-      </div>
-    </BrowserRouter>
-  </StrictMode>
+  <BrowserRouter>
+    <div className='min-w-screen'>
+      <AppRoutes />
+    </div>
+  </BrowserRouter>
 )
